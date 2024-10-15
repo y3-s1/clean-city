453ca7e084f6def081a214678018a4675c3c62ef
don99ishara <donishara99@gmail.com>
2024-10-15 13:50:47 +0530

    sdfsfd

--- src/pages/common/HomeResident.jsx
+++ src/pages/common/HomeResident.jsx
@@ -1,11 +1,79 @@
-import HomeResidentComponent from "../../components/common/HomeResidentComponent"
+import React from 'react';
+import { Container, Typography, Grid, Paper, Box } from '@mui/material';
+import { FaRecycle, FaTrash, FaGlobe, FaLightbulb } from 'react-icons/fa';
 
 const HomeResident = () => {
   return (
-    <div>
-        <HomeResidentComponent></HomeResidentComponent>
-    </div>
-  )
-}
+    <Container maxWidth="lg" className="mt-4">
+      <Box className="mb-4" style={{ height: '300px', overflow: 'hidden' }}>
+        <img
+          src="https://gargeon.com/wp-content/uploads/2022/09/best-waste-disposal-service.jpg"
+          alt="Waste Management"
+          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+        />
+      </Box>
 
-export default HomeResident
+      <Grid container spacing={4}>
+        {[
+          {
+            title: "Waste Monitoring",
+            content: [
+              "Remember to sort your recyclables!"
+            ],
+            icon: <FaRecycle size={24} />
+          },
+          {
+            title: "Waste Collection",
+            content: [
+              "Regular trash pickup: Every Monday and Thursday",
+              "Please place bins on the curb by 7 AM."
+            ],
+            icon: <FaTrash size={24} />
+          },
+          {
+            title: "Your Impact",
+            content: [
+              "Help to heal the world!"
+            ],
+            icon: <FaGlobe size={24} />
+          },
+          {
+            title: "Today's Tip",
+            content: [
+              "Use reusable shopping bags"
+            ],
+            icon: <FaLightbulb size={24} />
+          }
+        ].map((card, index) => (
+          <Grid item xs={12} md={6} key={index}>
+            <Paper 
+              className="p-4" 
+              style={{ 
+                height: '100%', 
+                display: 'flex', 
+                flexDirection: 'column',
+                backgroundColor: '#D3EE98'  // Pale light green color
+              }}
+            >
+              <Box display="flex" alignItems="center" className="mb-3">
+                {card.icon}
+                <Typography variant="h5" style={{ marginLeft: '8px' }}>
+                  {card.title}
+                </Typography>
+              </Box>
+              <Box flexGrow={1}>
+                {card.content.map((item, i) => (
+                  <Typography key={i} paragraph>
+                    {item}
+                  </Typography>
+                ))}
+              </Box>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default HomeResident;
